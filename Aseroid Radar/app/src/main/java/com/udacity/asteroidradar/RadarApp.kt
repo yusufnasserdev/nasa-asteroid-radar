65dadeb182aaa8cf27a19a339dd4302ac9748fd0
Yusuf Nasser <YusufNasserEng@gmail.com>
2023-06-01 03:55:54 +0300

    Add docs for RadarApp.kt & RefreshDataWorker.kt

--- Aseroid Radar/app/src/main/java/com/udacity/asteroidradar/RadarApp.kt
+++ Aseroid Radar/app/src/main/java/com/udacity/asteroidradar/RadarApp.kt
@@ -9,16 +9,35 @@ import kotlinx.coroutines.launch
 import java.util.concurrent.TimeUnit
 import timber.log.Timber
 
+/**
+ * RadarApp is the application class for the app.
+ *
+ * It is used to setup the recurring work for the app.
+ */
 
 class RadarApp : Application() {
 
+    /**
+     * onCreate() is called when the application is starting, before any other application objects have been created.
+     */
     override fun onCreate() {
         super.onCreate()
-        Timber.plant(Timber.DebugTree())
+        Timber.plant(Timber.DebugTree()) // Setup Timber for logging
         delayedInit()
     }
 
+    /**
+     * applicationScope is the [CoroutineScope] for the application.
+     * It is used to launch a new coroutine on the background thread.
+     */
+
     private val applicationScope = CoroutineScope(Dispatchers.Default)
+
+    /**
+     * delayedInit() is used to setup the recurring work for the app.
+     *
+     * it is delayed to avoid blocking the main thread.
+     */
 
     private fun delayedInit() {
         applicationScope.launch {
@@ -26,7 +45,20 @@ class RadarApp : Application() {
         }
     }
 
+    /**
+     * setupRecurringWork() sets up the recurring work for the app.
+     *
+     * It sets up the work to refresh the asteroids data in the database and the images
+     * on a daily basis. uses the [WorkManager] to enqueue the work and the [Constraints] to
+     * set the constraints for the work. The constraints are:
+     * 1. The network type should be unmetered
+     * 2. The battery should not be low
+     * 3. The device should be charging
+     * 4. The device should be idle
+     */
+
     private fun setupRecurringWork() {
+        // Setup the constraints for the work
         val constraints = Constraints.Builder()
             .setRequiredNetworkType(NetworkType.UNMETERED)
             .setRequiresBatteryNotLow(true)
@@ -35,10 +67,12 @@ class RadarApp : Application() {
                 setRequiresDeviceIdle(true)
             }.build()
 
+        // Setup the work to refresh the asteroids data in the database and the images on a daily basis
         val repeatingRequest = PeriodicWorkRequestBuilder<RefreshDataWorker>(1, TimeUnit.DAYS)
             .setConstraints(constraints)
             .build()
 
+        // Enqueue the work
         WorkManager.getInstance(applicationContext).enqueueUniquePeriodicWork(
             RefreshDataWorker.WORK_NAME,
             ExistingPeriodicWorkPolicy.KEEP,
--- Aseroid Radar/app/src/main/java/com/udacity/asteroidradar/work/RefreshDataWorker.kt
+++ Aseroid Radar/app/src/main/java/com/udacity/asteroidradar/work/RefreshDataWorker.kt
@@ -7,13 +7,31 @@ import com.udacity.asteroidradar.database.AppDatabase
 import com.udacity.asteroidradar.repository.AppRepository
 import retrofit2.HttpException
 
+
+/**
+ *
+ * RefreshDataWorker is a Worker class that refreshes the data in the database and the images on a daily basis.
+ *
+ * @param appContext: Context - The application [Context]
+ * @param params: WorkerParameters - Parameters to setup the internal state of this worker
+ */
+
 class RefreshDataWorker(appContext: Context, params: WorkerParameters) :
     CoroutineWorker(appContext, params) {
 
+    /**
+     * Companion object to define the [WORK_NAME] constant
+     */
     companion object {
         const val WORK_NAME = "RefreshDataWorker"
     }
 
+    /**
+     * doWork() is the main method of the Worker class. It is called asynchronously on a background thread provided by WorkManager.
+     * It refreshes the asteroids data in the database and the images on a daily basis.
+     *
+     *  @return Result - The result of the work, either [androidx.work.ListenableWorker.Result.Success] or [androidx.work.ListenableWorker.Result.retry]
+     */
     override suspend fun doWork(): Result {
         val database = AppDatabase.getInstance(applicationContext)
         val repository = AppRepository(database)
